ec516f94cbe34952368d76111a2cf6dc4ed321fc
Oscan099 <oscar.larsson@elev.ga.ntig.se>
2021-12-07 15:18:36 +0100

    Typ klart

---
+++ Advent-of-code2/src/AOC.java
@@ -0,0 +1,23 @@
+public class AOC {
+    public static void main(String[] args) {
+        int[] s = {16,1,2,0,4,2,7,1,2,14};
+        int a = 0;
+        int medel = 0;
+        int medv = 0;
+
+        for (int i = 0; i < s.length; i++){
+
+        }
+
+
+        //for(int i = 0; i < s.length; i++)  {
+        //   a += s[i];
+        //}
+        //medel = (a / s.length);
+        //JOptionPane.showMessageDialog(null, "The average is: " + medel);
+        //for(int p = 0; p < s.length; p++) {
+        //    medv += (Math.abs(s[p]- medel));
+        //}
+        //System.out.println(medv);
+    }
+}
